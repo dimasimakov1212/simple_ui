f925d2a460fef38cd3d2c336af8c900adaa9dccd
DimaSimakov <dimarodeo@gmail.com>
2024-02-25 18:07:08 +0700

    Add get_birds_list function2

--- handlers.py
+++ handlers.py
@@ -2,4 +2,4 @@ def get_birds_list(hashMap, _files=None, _data=None):
     if hashMap.get("listener") == "birds_list_btn":
         hashMap.put("toast", 'Список птиц')
 
-    return hashMap
+        return hashMap
